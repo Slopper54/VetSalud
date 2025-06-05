1c8cace3cfe68b98e64e8a7ec7b1e8ec81cd5bc0
danielferdiaz04 <154264060+danielferdiaz04@users.noreply.github.com>
2025-06-05 20:22:20 +0200

    Add files via upload

---
+++ src/java/entidades/Factura.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author samuel
+ */
+@Entity
+@Table(name = "factura")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Factura.findAll", query = "SELECT f FROM Factura f")
+    , @NamedQuery(name = "Factura.findById", query = "SELECT f FROM Factura f WHERE f.id = :id")
+    , @NamedQuery(name = "Factura.findByFechaEmision", query = "SELECT f FROM Factura f WHERE f.fechaEmision = :fechaEmision")
+    , @NamedQuery(name = "Factura.findByTotal", query = "SELECT f FROM Factura f WHERE f.total = :total")
+    , @NamedQuery(name = "Factura.findByMetodoPago", query = "SELECT f FROM Factura f WHERE f.metodoPago = :metodoPago")})
+public class Factura implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha_emision")
+    @Temporal(TemporalType.DATE)
+    private Date fechaEmision;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "total")
+    private BigDecimal total;
+    @Size(max = 50)
+    @Column(name = "metodo_pago")
+    private String metodoPago;
+    @JoinColumn(name = "id_cita", referencedColumnName = "id")
+    @OneToOne
+    private Cita idCita;
+
+    public Factura() {
+    }
+
+    public Factura(Integer id) {
+        this.id = id;
+    }
+
+    public Factura(Integer id, Date fechaEmision) {
+        this.id = id;
+        this.fechaEmision = fechaEmision;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Date fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public Cita getIdCita() {
+        return idCita;
+    }
+
+    
+    public void setIdCita(Cita idCita) {
+        this.idCita = idCita;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Factura)) {
+            return false;
+        }
+        Factura other = (Factura) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.Factura[ id=" + id + " ]";
+    }
+    
+}
