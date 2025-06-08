38c291c5af437b0a7ae63f08506aa96328432294
domingovm8 <104068493+domingovm8@users.noreply.github.com>
2025-06-08 13:39:50 +0200

    Vistas definitivas

---
+++ VetSalud/build/generated-sources/ap-source-output/entidad/Factura_.java
@@ -0,0 +1,20 @@
+package entidad;
+
+import entidad.Cita;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2025-06-08T13:00:07")
+@StaticMetamodel(Factura.class)
+public class Factura_ { 
+
+    public static volatile SingularAttribute<Factura, String> metodoPago;
+    public static volatile SingularAttribute<Factura, Cita> idCita;
+    public static volatile SingularAttribute<Factura, BigDecimal> total;
+    public static volatile SingularAttribute<Factura, Date> fechaEmision;
+    public static volatile SingularAttribute<Factura, Integer> id;
+
+}
